c69154aa998d4f450775a7ed3eaf537f699a3540
Luiz F A Oliveira <78864401+luizfao@users.noreply.github.com>
2024-09-24 16:00:18 -0300

    add operator secrets

--- 6-install-hum-operator.sh
+++ 6-install-hum-operator.sh
@@ -9,6 +9,24 @@ helm install humanitec-operator \
   --namespace humanitec-operator-system \
   --create-namespace
 
+# Operator secret keys - Config auth for drivers https://developer.humanitec.com/integration-and-extensions/humanitec-operator/installation/#configure-authentication-for-drivers
+# Generate a new private key
+openssl genpkey -algorithm RSA -out humanitec_operator_private_key.pem -pkeyopt rsa_keygen_bits:4096
+
+# Extract the public key from the private key generated in the previous command
+openssl rsa -in humanitec_operator_private_key.pem -outform PEM -pubout -out humanitec_operator_public_key.pem
+
+# create the secret
+oc -n humanitec-operator-system create secret generic humanitec-operator-private-key \
+     --from-file=privateKey=humanitec_operator_private_key.pem \
+     --from-literal=humanitecOrganisationID=$HUMANITEC_ORG
+
+# register in humanitec
+humctl api post /orgs/${HUMANITEC_ORG}/keys \
+  -d "$(cat humanitec_operator_public_key.pem | jq -sR)"
+
+echo register operator public key exit code=$?
+
 oc get pods -n humanitec-operator-system
 
 echo done
